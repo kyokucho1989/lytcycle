0083bce50248fe86fbc73235f284ef703287d686
ko <kyokucho1989@gmail.com>
2024-04-26 17:55:24 +0900

    animejsを導入

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,4 +1,5 @@
 import { Application } from "@hotwired/stimulus"
+import anime from "animejs"
 
 const application = Application.start()
 
@@ -7,3 +8,11 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+anime({
+  targets: 'div',
+  translateX: 250,
+  rotate: '1turn',
+  backgroundColor: '#FFF',
+  duration: 800
+});
