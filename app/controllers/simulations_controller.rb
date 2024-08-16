1e76ccfbebd90779e90fabaea6327ebd17406f2d
ko <kyokucho1989@gmail.com>
2024-08-17 08:04:58 +0900

    ルーティング修正

--- app/controllers/simulations_controller.rb
+++ app/controllers/simulations_controller.rb
@@ -1,6 +1,10 @@
 class SimulationsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user! except: :new
   def index
     @simulations = Simulation.all
+  end
+
+  def new
+
   end
 end
