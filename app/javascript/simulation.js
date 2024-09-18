48cb295a40afa4a5d1094986821e84257b47f2be
ko <kyokucho1989@gmail.com>
2024-09-19 04:35:49 +0900

    パラメータの保存機能を追加

--- app/javascript/simulation.js
+++ app/javascript/simulation.js
@@ -127,54 +127,43 @@ export async function drawLink(linksData, nodesData) {
       });
   }
 }
-//
-// const showButton = document.getElementById("showDialog");
-const favDialog = document.getElementById("favDialog");
-const outputBox = document.querySelector("output");
-const selectEl = favDialog.querySelector("select");
-const confirmBtn = favDialog.querySelector("#confirmBtn");
 
-// // "Show the dialog" ボタンで <dialog> をモーダルに開く
-// showButton.addEventListener("click", () => {
-//   favDialog.showModal();
-// });
+let facilityDialog, confirmBtn;
+document.addEventListener("turbo:load", async () => {
+  facilityDialog = document.getElementById("facilityDialog");
+  confirmBtn = document.getElementById("confirmBtn");
 
-// "Favorite animal" 入力で、送信ボタンの値を設定する
-selectEl.addEventListener("change", (e) => {
-  confirmBtn.value = selectEl.value;
+  // ［確認］ボタンが既定でフォームを送信しないようにし、`close()` メソッドでダイアログを閉じ、"close" イベントを発生させる
+  confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // この偽フォームを送信しない
+    let id = document.getElementById("hidden-id");
+    let name = document.getElementById("name");
+    let processingTime = document.getElementById("processingTime");
+
+    let selectedFacility = facilities.find(
+      (facility) => facility.id == id.value
+    );
+    selectedFacility.name = name.value;
+    selectedFacility.processingTime = processingTime.value;
+
+    facilityDialog.close();
+  });
 });
-
-// "Cancel" ボタンで [formmethod="dialog"] による送信を行わずにダイアログを閉じ、close イベントを発行する
-favDialog.addEventListener("close", (e) => {
-  console.log(this);
-  outputBox.value =
-    favDialog.returnValue === "default"
-      ? "No return value."
-      : `ReturnValue: ${favDialog.returnValue}.`; // 空文字列ではなく、既定値かどうかを調べる必要がある
-});
-
-// ［確認］ボタンが既定でフォームを送信しないようにし、`close()` メソッドでダイアログを閉じ、"close" イベントを発生させる
-confirmBtn.addEventListener("click", (event) => {
-  event.preventDefault(); // この偽フォームを送信しない
-  favDialog.close(selectEl.value); // ここで選択ボックスの値を送る必要がある
-});
-
-//
 function nodeClicked() {
-  // alert("click");
-  // console.log(this);
-
   let facilityForEdit = facilities.find((facility) => facility.id == this.id);
   console.log(facilityForEdit);
   setFacilityDataToModal(facilityForEdit);
-  favDialog.showModal();
+  facilityDialog.showModal();
 }
 
 function setFacilityDataToModal(facility) {
-  const name = document.getElementById("name");
+  let id = document.getElementById("hidden-id");
+  let name = document.getElementById("name");
+  let processingTime = document.getElementById("processingTime");
+
+  id.value = facility.id;
   name.value = facility.name;
-  console.log(outputBox);
-  console.log(facility);
+  processingTime.value = facility.processingTime;
 }
 
 function dragstarted(event) {
