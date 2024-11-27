b524b540d261f7676c3d851f09e5dc2b4a0ed8fe
kyokucho1989 <kyokucho1989@gmail.com>
2024-11-27 16:46:26 +0900

    利用規約ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   end
   resources :simurates
   get 'demo' , to: 'simulations#demo'
+
+  resource :term, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
