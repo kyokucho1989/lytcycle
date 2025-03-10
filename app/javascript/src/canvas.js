a7d3c73cb0999d9af328bbb2757a2b5b386ab36f
ko <kyokucho1989@gmail.com>
2025-03-11 06:00:20 +0900

    色を変えるメソッド追加

--- app/javascript/src/canvas.js
+++ app/javascript/src/canvas.js
@@ -46,7 +46,6 @@ export async function drawLink(linksData, nodesData) {
         .enter()
         .append("line")
         .attr("stroke-width", (d) => (d.id.includes("re") ? 0 : 8))
-        .attr("stroke", "black")
         .attr("id", function (d) {
           return d.id;
         })
@@ -62,11 +61,12 @@ export async function drawLink(linksData, nodesData) {
           return d.r;
         })
         .attr("stroke", "black")
-        .attr("fill", "LightSalmon")
         .attr("id", function (d) {
           return d.id;
         });
 
+      setNodeColor(nodesData, "#99aaee");
+      setLinkColor(linksData, "#999");
       // シミュレーション描画
       simulation = d3
         .forceSimulation()
@@ -137,6 +137,14 @@ export async function drawLink(linksData, nodesData) {
       resolve(); // SVGが存在しない場合もresolveを呼び出す
     }
   });
+}
+
+export async function setNodeColor(nodesData, color) {
+  d3.select("#svg02").selectAll("circle").data(nodesData).attr("fill", color);
+}
+
+export async function setLinkColor(linksData, color) {
+  d3.selectAll("line").data(linksData).attr("stroke", color);
 }
 
 export function nodeClicked() {
