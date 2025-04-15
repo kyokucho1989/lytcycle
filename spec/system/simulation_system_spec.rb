2cab23d9aa51c013139220ec1b6ca290ca3029ac
ko <kyokucho1989@gmail.com>
2025-04-16 05:30:30 +0900

    テスト用のユーザーを認証済として作成

--- spec/system/simulation_system_spec.rb
+++ spec/system/simulation_system_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Widget management', type: :system do
   before do
     driven_by(:rack_test)
-    @user = User.create!(name: 'satou2', email: 'satou2@example.com', password: 'password')
+    @confirmed_user = User.create!(name: 'satou2', email: 'satou2@example.com', password: 'password', confirmed_at: DateTime.now)
   end
 
   it 'visiting the demo page' do
@@ -12,8 +12,8 @@ RSpec.describe 'Widget management', type: :system do
   end
 
   it 'visiting the index simulation when sign in' do
-    sign_in @user
-    visit user_simulations_path(@user)
+    sign_in @confirmed_user
+    visit user_simulations_path(@confirmed_user)
     assert_selector 'h1', text: 'シミュレーション一覧'
   end
 
