b2c411d114e702167c8d8e2eaf8362640301ca48
kyokucho1989 <kyokucho1989@gmail.com>
2025-04-24 12:29:18 +0900

    編集モードと実行モードを統一

--- app/javascript/controllers/simulationmode_controller.js
+++ app/javascript/controllers/simulationmode_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "@hotwired/stimulus";
-// import { changeInactiveObject } from "src/simulation";
 import { changeActiveObject, drawLink, activePlayButtons } from "src/canvas";
 import { setClickEventToObject } from "src/simulation";
 import { routes } from "src/set_simulation_params";
@@ -7,13 +6,7 @@ import { findInvalidRouteIds } from "src/consistency_check";
 import { countStart } from "src/exec_simulation";
 
 export default class extends Controller {
-  static targets = ["edit", "simulate", "editbutton", "simulatebutton"];
-
-  // 状態の定義
-  MAIN_STATES = {
-    EDIT: "edit",
-    RUNNING: "running",
-  };
+  static targets = ["editbutton", "simulatebutton"];
 
   STATES = {
     SELECT: "select",
@@ -24,49 +17,32 @@ export default class extends Controller {
   };
 
   connect() {
-    this.mainState = this.MAIN_STATES.EDIT;
     this.state = this.STATES.SELECT;
-    // this.readyForExecution = false;
-    this.editTarget.hidden = false;
-    this.simulateTarget.hidden = false;
-    // this.editbuttonTarget.hidden = true;
-    // this.simulatebuttonTarget.hidden = false;
   }
 
   edit() {
-    this.mainState = this.MAIN_STATES.EDIT;
     this.state = this.STATES.SELECT;
     console.log(this.mainState, this.state);
-    this.editTarget.hidden = false;
-    this.simulateTarget.hidden = false;
-    // this.editbuttonTarget.hidden = true;
-    // this.simulatebuttonTarget.hidden = false;
-    const editSubRadio = document.getElementById("select");
-    editSubRadio.checked = true;
     changeActiveObject();
     setClickEventToObject(this);
   }
 
   changeModeToAddMan() {
-    // this.mainState = this.MAIN_STATES.EDIT;
     this.state = this.STATES.ADD_OPERATOR;
     setClickEventToObject(this);
   }
 
   changeModeToAddFacility() {
-    // this.mainState = this.MAIN_STATES.EDIT;
     this.state = this.STATES.ADD_FACILITY;
     setClickEventToObject(this);
   }
 
   changeModeToLink() {
-    // this.mainState = this.MAIN_STATES.EDIT;
     this.state = this.STATES.LINK;
     setClickEventToObject(this);
   }
 
   changeModeToDelete() {
-    // this.mainState = this.MAIN_STATES.EDIT;
     this.state = this.STATES.DELETE;
     setClickEventToObject(this);
   }
@@ -82,12 +58,6 @@ export default class extends Controller {
     this.readyForExecution = true;
     await countStart();
     activePlayButtons();
-    // const play = document.getElementById("play");
-    // const pause = document.getElementById("pause");
-    // const progress = document.getElementById("progress");
-    // play.disabled = false;
-    // pause.disabled = false;
-    // progress.disabled = false;
   }
 
   checkSimulate() {
@@ -110,8 +80,6 @@ export default class extends Controller {
     console.log(this.mainState, this.state);
     this.editTarget.hidden = true;
     this.simulateTarget.hidden = false;
-    // this.editbuttonTarget.hidden = false;
-    // this.simulatebuttonTarget.hidden = true;
     setClickEventToObject(this);
   }
 }
