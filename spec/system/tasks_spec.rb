cfa5c03167c7424e4524559c3f4ff0a156b3935c
ko <kyokucho1989@gmail.com>
2025-04-29 07:03:41 +0900

    クリックイベントのテストを追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -18,4 +18,23 @@ RSpec.describe 'Widget', type: :system do
     expect(page).to have_css 'circle#n1'
     expect(page).to have_css 'circle#goal'
   end
+
+  it 'can edit facility attributes', js: true do
+    sign_in @confirmed_user
+    visit new_user_simulation_path(@confirmed_user)
+    find('circle#n1').click
+    fill_in '設備名' , with: '変更後の設備名' ,fill_options: { clear: :backspace }
+    fill_in '加工時間' , with: '30' ,fill_options: { clear: :backspace }
+    click_on '保存'
+    expect(page).to have_css 'circle#n1'
+  end
+
+  it 'can edit link attributes', js: true do
+    sign_in @confirmed_user
+    visit new_user_simulation_path(@confirmed_user)
+    find('line#root1-1').click
+    fill_in '距離' , with: '30' ,fill_options: { clear: :backspace }
+    click_on '保存'
+    expect(page).to have_css 'line#root1-1'
+  end
 end
