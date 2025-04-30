489ef7b6e622c63cb6790e863204a558ad688376
ko <kyokucho1989@gmail.com>
2025-04-30 09:40:15 +0900

    rspecが通るように修正

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Widget', type: :system do
+RSpec.describe 'RenderSVG', type: :system do
   before do
     @confirmed_user = User.create!(name: 'satou2', email: 'satou2@example.com', password: 'password',
                                    confirmed_at: DateTime.now)
@@ -18,13 +18,20 @@ RSpec.describe 'Widget', type: :system do
     expect(page).to have_css 'circle#n1'
     expect(page).to have_css 'circle#goal'
   end
+end
+
+RSpec.describe 'EditObjects', type: :system do
+  before do
+    @confirmed_user = User.create!(name: 'satou2', email: 'satou2@example.com', password: 'password',
+                                   confirmed_at: DateTime.now)
+  end
 
   it 'can edit facility attributes', js: true do
     sign_in @confirmed_user
     visit new_user_simulation_path(@confirmed_user)
     find('circle#n1').click
-    fill_in '設備名' , with: '変更後の設備名' ,fill_options: { clear: :backspace }
-    fill_in '加工時間' , with: '30' ,fill_options: { clear: :backspace }
+    fill_in '設備名', with: '変更後の設備名', fill_options: { clear: :backspace }
+    fill_in '加工時間', with: '30', fill_options: { clear: :backspace }
     click_on '保存'
     expect(page).to have_css 'circle#n1'
   end
@@ -33,7 +40,7 @@ RSpec.describe 'Widget', type: :system do
     sign_in @confirmed_user
     visit new_user_simulation_path(@confirmed_user)
     find('line#root1-1').click
-    fill_in '距離' , with: '30' ,fill_options: { clear: :backspace }
+    fill_in '距離', with: '30', fill_options: { clear: :backspace }
     click_on '保存'
     expect(page).to have_css 'line#root1-1'
   end
