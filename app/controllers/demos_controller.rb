ec7a3ce97a75b0890bb6bba900aa642506b3b525
ko <kyokucho1989@gmail.com>
2025-05-31 04:25:01 +0900

    ログイン状態のリダイレクト先を適切に修正
    ログイン時はデモページからシミュレーション一覧へリダイレクトするよう修正

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -1,6 +1,9 @@
 class DemosController < ApplicationController
   def new
-    redirect_to new_user_simulation_path(current_user.id) if user_signed_in?
+    if user_signed_in?
+      redirect_to new_user_simulation_path(current_user.id)
+      return
+    end
     @simulation = Simulation.new
     render 'simulations/demo'
   end
