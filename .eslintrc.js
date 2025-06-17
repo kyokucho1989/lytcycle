551e306a1b3b844cf7d1be66f24768f9ccff8524
ko <kyokucho1989@gmail.com>
2025-06-18 05:50:45 +0900

    prettier導入と適用

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
   },
   rules: {
     "no-fallthrough": "off",
+     "eqeqeq": "error",
   },
 };
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/*
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+}
+
+export default config
