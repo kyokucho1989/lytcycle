9500ac59df5db729d60ad2fbfdee1fc8d0f509e7
ko <kyokucho1989@gmail.com>
2025-07-04 04:40:58 +0900

    testをRspecに移した

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
+RSpec.describe 'EditObjects', type: :system do
+  it 'can edit facility attributes', js: true do
+    visit new_simulation_path
+    sleep 3
+    find('circle#n1').click
+    sleep 3
+    fill_in '設備名', with: '変更後の設備名', fill_options: { clear: :backspace }
+    fill_in '加工時間', with: '30', fill_options: { clear: :backspace }
+    click_on '保存'
+    expect(page).to have_field '設備名', with: '変更後の設備名'
+    expect(page).to have_field '加工時間', with: '30'
+  end
+
+  it 'can edit link attributes', js: true do
+    visit new_simulation_path
+    sleep 5
+    find('line#root1-1').click
+    fill_in '距離', with: '30', fill_options: { clear: :backspace }
+    click_on '保存'
+    sleep 5
+    find('line#root1-1').click
+    expect(page).to have_field '距離', with: 30
+  end
+end
+
 RSpec.describe 'RenderSVG', type: :system do
   before do
     @confirmed_user = User.create!(name: 'satou2', email: 'satou2@example.com', password: 'password',
@@ -17,36 +42,6 @@ RSpec.describe 'RenderSVG', type: :system do
     expect(page).to have_css 'circle#start'
     expect(page).to have_css 'circle#n1'
     expect(page).to have_css 'circle#goal'
-  end
-end
-
-RSpec.describe 'EditObjects', type: :system do
-  # before do
-  #   @confirmed_user = User.create!(name: 'satou2', email: 'satou2@example.com', password: 'password',
-  #                                  confirmed_at: DateTime.now)
-  # end
-
-  it 'can edit facility attributes', js: true do
-    visit new_simulation_path
-    sleep 3
-    find('circle#n1').click
-    sleep 3
-    fill_in '設備名', with: '変更後の設備名', fill_options: { clear: :backspace }
-    fill_in '加工時間', with: '30', fill_options: { clear: :backspace }
-    click_on '保存'
-    expect(page).to have_css 'circle#n1'
-  end
-
-  it 'can edit link attributes', js: true do
-    visit new_simulation_path
-    sleep 5
-    find('line#root1-1').click
-    sleep 5
-    fill_in '距離', with: '30', fill_options: { clear: :backspace }
-    click_on '保存'
-    sleep 5
-    find('line#root1-1').click
-    expect(page).to have_css('line#root1-1', wait: 5, visible: :all)
   end
 end
 
